783bc16b55999679a536c66ccc667aab3556af81
Emil Österlund <emil@larsemil.se>
2022-08-23 12:12:45 +0200

    Updated hello world page

--- Views/hello.blade.php
+++ Views/hello.blade.php
@@ -23,8 +23,18 @@
                 Därför finns <strong>routing</strong>, <strong>controllers</strong>, <strong> views </strong> och <strong>modeller</strong>.
 
             </p>
-            <p>Allt är composerbaserat och modulärt med inlånade saker från andra ramverk. 
+            <p>Allt är composerbaserat och modulärt med inlånade saker från andra ramverk. (<i>Blade</i>, <i>dd</i>, <i>Whoops</i>)
             
+        </div>
+
+        <div class="right">
+            <h3>Komma igång: </h3>
+                <ul>
+                    <li>Börja i <strong>Routes.php</strong></li>
+                    <li>Där finns exempel på några olika sorters routes.</li>
+                    <li>De länkar till en <strong>Controller</strong>.</li>
+                    <li>Controllern i sin tur kan använda en <strong>View</strong> för att presentera innehållet.</li>
+                </ul>
         </div>
 
     </div>
